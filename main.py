6e1d3c1927a6bcd6955b18a9996befba30486141
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-25 15:49:46 -0600

    Ejercicio 5 + mejoras del ejercicio 1

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ while True:
                         titulo = input(f"\nIngrese el título de la película {i+1}: ")
                         año = int(input(f"Ingrese la fecha de lazamiento: "))
                         genero = input("Ingrese el género de la película: ").lower()
+                        if len(peliculas) > 0:
+                            for pelicula in peliculas:
+                                if pelicula["titulo"] == titulo:
+                                    print("Ya hay una película con ese nombre")
+                                    break
                         break
                     except:
                         print("Vuelva a intentarlo por favor")
@@ -35,12 +40,26 @@ while True:
         case "3":
             genre_search = input("\nIngrese el género a buscar: ").lower()
             print("Películas que coinciden con ese género: ")
+            match = False
             for pelicula in peliculas:
                 if pelicula["genre"] == genre_search:
+                    match = True
                     print("\nNombre: "+pelicula["título"] + "\nLanzamiento:" + pelicula["año"] + "\nGénero: " + pelicula["género"])
+            if not match:
+                print("No hay películas con ese género")
 
         case "4":
-            pass
+            titulo_delete = input("\nIngrese el título de la película que desea eliminar: ")
+            match2 = False
+            for pelicula in peliculas:
+                if titulo_delete == pelicula["título"]:
+                    match2 = True
+                    peliculas.remove(pelicula)
+                    print("Película eliminada")
+                    break
+            if not match2:
+                print("No hay una película con ese nombre en la lista")
+
         case "5":
             print("\nSaliendo...")
             break
