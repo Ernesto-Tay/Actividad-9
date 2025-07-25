c108c107806efa6bce472cf4134e138d67e2fcd2
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-25 15:33:58 -0600

    Opción 1

--- main.py
+++ main.py
@@ -4,7 +4,30 @@ while True:
     selection = input("Seleccione una opción: ")
     match selection:
         case "1":
-            pass
+            while True:
+                try:
+                    cant = int(input("\n¿Cuántas películas va a ingresar?: "))
+                    if cant<=0:
+                        print("La cantidad debe ser mayor a 0")
+                    else:
+                        break
+                except:
+                    print("Ingrese un numero entero")
+
+            for i in range(cant):
+                while True:
+                    try:
+                        title = input(f"\nIngrese el título de la película {i+1}: ")
+                        lauch = int(input(f"Ingrese la fecha de lazamiento: "))
+                        genre = input("Ingrese el género de la película: ")
+                        break
+                    except:
+                        print("Vuelva a intentarlo por favor")
+
+            peli = {"title": title,"lauch": lauch,"genre": genre,}
+            peliculas.append(peli)
+            print("Película añadida con éxito")
+
         case "2":
             pass
         case "3":
